0064cd06b4448b321465b1e9aa44299f850e2eb8
DiligentP <qkrwjdgus0603@gmail.com>
2024-06-12 20:22:15 +0900

    init Spring Project

---
+++ src/main/java/com/example/mannazo/MannazoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mannazo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MannazoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MannazoApplication.class, args);
+    }
+
+}
