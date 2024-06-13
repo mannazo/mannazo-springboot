01cde04211e3ebb4cada22c347e568e92cb9a970
DiligentP <qkrwjdgus0603@gmail.com>
2024-06-13 18:00:13 +0900

    init Springboot

---
+++ src/main/java/com/mannazo/mannazo/Swagger/SwaggerController.java
@@ -0,0 +1,17 @@
+package com.mannazo.mannazo.Swagger;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class SwaggerController {
+    /**
+     * 루트 경로(/) 접근을 Swagger Page로 리다이렉션합니다.
+     */
+    @GetMapping("/")
+    public String redirectSwagger() {
+        return "redirect:/swagger-ui/index.html";
+    }
+}
