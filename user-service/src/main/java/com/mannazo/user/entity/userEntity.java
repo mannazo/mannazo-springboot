faf6f1bce5c5b4dee5f5ea28635dd1f665d46709
root <root@test.pri>
2024-07-04 11:37:57 +0900

    Feat : user-service CRUD 기본구조 생성

--- user-service/src/main/java/com/mannazo/user/entity/userEntity.java
+++ user-service/src/main/java/com/mannazo/user/entity/userEntity.java
@@ -1,10 +1,15 @@
 package com.mannazo.user.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class userEntity {
 
+    @Id
+    @GeneratedValue
+    private int id;
 
 
 }
