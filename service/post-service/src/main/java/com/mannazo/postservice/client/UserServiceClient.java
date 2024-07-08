bedf5ce34ab425d0301414c97af62cddcf04deb6
kimdongho <kdh96331@naver.com>
2024-07-08 11:58:18 +0900

    Feat: PostService에 FeignClient 추가 및 gradle wrapper 삭제

--- service/post-service/src/main/java/com/mannazo/postservice/client/UserServiceClient.java
+++ service/post-service/src/main/java/com/mannazo/postservice/client/UserServiceClient.java
@@ -7,8 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-
-
 @FeignClient(name = "user-service", url = "http://localhost:8080/user")
 public interface UserServiceClient {
 
