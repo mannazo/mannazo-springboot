4252f7cfb7de52e87dd101b8631bde103bdd4bc7
DiligentP <qkrwjdgus0603@gmail.com>
2024-07-08 18:38:38 +0900

    Feat: 카카오 로그인 구현

---
+++ service/auth-service/src/main/java/com/mannazo/auth/util/JwtUtil.java
@@ -0,0 +1,110 @@
+package com.mannazo.auth.util;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * [JWT 관련 메서드를 제공하는 클래스]
+ */
+@Slf4j
+@Component
+public class JwtUtil {
+    private final String secretKey;
+    private final long accessTokenExpTime;
+    private final Key key;
+
+    @Autowired
+    public JwtUtil(@Value("${jwt.secret}") String secretKey, @Value("${jwt.expiration_time}") long accessTokenExpTime) {
+        this.secretKey = secretKey;
+        this.accessTokenExpTime = accessTokenExpTime;
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    /**
+     * Access Token 생성
+     * @param userid
+     * @return Access Token String
+     */
+    public String createAccessToken(UUID userid) {
+        return createToken(userid, accessTokenExpTime);
+    }
+
+    /**
+     * JWT 생성
+     * @param userid
+     * @param expireTime
+     * @return JWT String
+     */
+    private String createToken(UUID userid, long expireTime) {
+        Claims claims = Jwts.claims();
+        claims.put("userid", userid);
+
+        ZonedDateTime now = ZonedDateTime.now();
+        ZonedDateTime tokenValidity = now.plusSeconds(expireTime);
+
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(Date.from(now.toInstant()))
+                .setExpiration(Date.from(tokenValidity.toInstant()))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+
+    /**
+     * Token에서 User ID 추출
+     * @param token
+     * @return User ID
+     */
+    public UUID getUserId(String token) {
+        return parseClaims(token).get("userid", UUID.class);
+    }
+
+
+    /**
+     * JWT 검증
+     * @param token
+     * @return IsValidate
+     */
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("Invalid JWT Token", e);
+        } catch (ExpiredJwtException e) {
+            log.info("Expired JWT Token", e);
+        } catch (UnsupportedJwtException e) {
+            log.info("Unsupported JWT Token", e);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT claims string is empty.", e);
+        }
+        return false;
+    }
+
+
+    /**
+     * JWT Claims 추출
+     * @param accessToken
+     * @return JWT Claims
+     */
+    public Claims parseClaims(String accessToken) {
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
+        } catch (ExpiredJwtException e) {
+            return e.getClaims();
+        }
+    }
+
+
+}
