bcbcdb402369dc2f6899f4e383aa9b6f6f322a55
DiligentP <qkrwjdgus0603@gmail.com>
2024-07-11 02:50:49 +0900

    Feat : 모든 서비스의 BootJar를 수행하는 쉘스크립트 작성

---
+++ buildModules.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# IntelliJ IDEA JDK 경로, 실제 경로로 변경해주세요
+export JAVA_HOME="C:\\Users\\dilig\\.jdks\\corretto-17.0.11"
+
+# 모듈 리스트
+modules=("server:api-gateway" "server:config-server" "server:eureka-server"
+"service:user-service" "service:auth-service" "service:chat-service"
+"service:admin-service" "service:post-service" "service:shop-service"
+"service:community-service")
+
+# Gradle clean
+echo "Cleaning..."
+./gradlew clean
+
+# Gradle BootJar
+for module in "${modules[@]}"
+do
+  echo "Building BootJar for $module"
+  ./gradlew :$module:bootJar
+done
