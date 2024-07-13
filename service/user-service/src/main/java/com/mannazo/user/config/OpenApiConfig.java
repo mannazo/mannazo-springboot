6ae980d109c30fd49a81494185508a49e0749a04
DOLONG9 <bagmy2@gmail.com>
2024-07-13 15:22:30 +0900

    Feat : OpenApiConfig 생성 for Swagger

---
+++ service/user-service/src/main/java/com/mannazo/user/config/OpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.mannazo.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/user"));
+    }
+}
